a21680f433f85537000c8a486cc24471eae440ab
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-08-20 23:17:56 +0530

    added

--- first.js
+++ first.js
@@ -19,8 +19,15 @@ const React ={
     }
  }
 
- 
+
 const header1 = React.createElement('h1',{fontSize:"30px", backgroundColor:"blue", color:"white"},"Hello coder dosto");
 const header2 = React.createElement('h2',{fontSize:"30px", backgroundColor:"blue", color:"white"},"kase ho app");
 
 document.getElementById("root").append(header1, header2);
+
+// const root = document.getElementById('root');
+// root.append(header1);
+// root.append(header2);
+
+ReactDOM.render(header1,document.getElementById('root'));
+ReactDOM.render(header1,document.getElementById('root'));
